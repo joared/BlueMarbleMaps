5edeab281a082fc69ef1ee23d6cbfbf5e1881c64
Joar <joared@kth.se>
2025-09-21 21:24:28 +0200

    Started on test for FeatureDatabase

---
+++ examples/test_filedatabase.cpp
@@ -0,0 +1,19 @@
+#include "BlueMarbleMaps/Core/Index/FileDatabase.h"
+
+using namespace BlueMarble;
+
+void populateDatabase(IFeatureDataBase* db)
+{
+
+    auto line = std::make_shared<LineGeometry>();
+    //auto f1 = std::make_shared<Feature>();
+
+}
+
+int main() 
+{
+    auto database = FileDatabase();
+
+
+    return 0;
+}
