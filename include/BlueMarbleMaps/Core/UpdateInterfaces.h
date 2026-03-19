e32126e111e88e30fc833f3d7e5ff29a40b62250
Joar <joared@kth.se>
2026-03-19 17:02:07 +0100

    Minor cleanup of unused code

--- include/BlueMarbleMaps/Core/UpdateInterfaces.h
+++ include/BlueMarbleMaps/Core/UpdateInterfaces.h
@@ -38,12 +38,12 @@ namespace BlueMarble
             IdCollectionPtr ids() const { return m_ids; }
 
         private:
-            Rectangle   m_area = Rectangle::infinite();
-            IdCollectionPtr m_ids = std::make_shared<IdCollection>();
-            double      m_scale = 1.0;
-            Attributes* m_updateAttributes = nullptr;
-            bool        m_quickUpdate = false;
-            RasterGeometryMode m_rasterMode = RasterGeometryMode::Original;
+            Rectangle           m_area = Rectangle::infinite();
+            IdCollectionPtr     m_ids = std::make_shared<IdCollection>();
+            double              m_scale = 1.0;
+            Attributes*         m_updateAttributes = nullptr;
+            bool                m_quickUpdate = false;
+            RasterGeometryMode  m_rasterMode = RasterGeometryMode::Original;
             double              m_resolution=-1.0;
     };
 
@@ -69,39 +69,6 @@ namespace BlueMarble
             int m_iterationIndex;
             FeatureCollectionPtr m_features;
             std::vector<FeatureEnumeratorPtr> m_subEnumerators;
-    };
-
-    // Downstream
-    // Interface for layers, operators and data sets that handles update requests in the operator chain
-    class IUpdateHandler
-    {
-        public:
-            // New stuff
-            // virtual const FeatureEnumeratorPtr& getFeatures(const FeatureQuery& query) = 0;
-            // Old
-            virtual void onUpdateRequest(Map& map, const Rectangle& updateArea, FeatureHandler* handler) = 0;
-            virtual void onGetFeaturesRequest(const Attributes& attributes, std::vector<FeaturePtr>& features) = 0;
-            virtual FeaturePtr onGetFeatureRequest(const Id& id) = 0;
-            virtual ~IUpdateHandler() = default;
-
-    };
-
-    // Upstream
-    // Interface for layers, operators and data sets that handles features as a results of and update request
-    class FeatureHandler
-    {
-        public:
-            FeatureHandler();
-            // TODO: we send "back" a reference to the map since layers needs it to render
-            virtual void onFeatureInput(Map &map, const std::vector<FeaturePtr>& features) = 0;
-            virtual ~FeatureHandler() = default;
-            void addUpdateHandler(IUpdateHandler* handler);
-        protected:
-            void sendUpdateRequest(Map& map, const Rectangle& updateArea);
-            void sendGetFeaturesRequest(const Attributes& attributes, std::vector<FeaturePtr>& features);
-            FeaturePtr sendGetFeatureRequest(const Id& id);
-        private:
-            std::vector<IUpdateHandler*> m_updateHandlers;
     };
 }
 
